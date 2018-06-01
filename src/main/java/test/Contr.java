b2222ef55c68cbdd793e11e31ed9e5cad0d6a9ac
liu2432773200 <2432773200@qq.com>
2018-06-01 17:45:59 +0800

    第一次提交

---
+++ src/main/java/test/Contr.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@RestController
+public class Contr {
+	@RequestMapping("/index")
+	public String index() {
+		// TODO Auto-generated method stub
+		System.out.println("、");
+		return "sdsddsdsds";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Contr.class, args);
+	}
+}
