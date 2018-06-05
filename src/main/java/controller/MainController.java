d097a727c5c141527ec1b6c7fe3655be1bf92e90
liu2432773200 <2432773200@qq.com>
2018-06-05 14:34:19 +0800

    第一次提交

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -2,9 +2,9 @@ package controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
-@ComponentScan("test")
+@ComponentScan(basePackages={"test","service"})
 public class MainController {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MainController.class, args);
 	}
--- src/main/java/test/Contr.java
+++ src/main/java/test/Contr.java
@@ -1,20 +1,31 @@
 package test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import service.UserService;
+
 @EnableAutoConfiguration
 @Controller
 public class Contr {
+	@Autowired
+	private UserService user;
 	
 	@RequestMapping("/index")
-	public String index() {
+	public String index(String name) {
+		if(name!=null){
+			if(name.length()!=0){
+				if(user.add(name)>0){
+					System.out.println("添加成功");
+				}else{
+					System.out.println("添加失败");
+				}
+			}
+		}
 		// TODO Auto-generated method stub
 		System.out.println("、");
 		return "MyHtml";
 	}
-	/*public static void main(String[] args) {
-		SpringApplication.run(Contr.class, args);
-	}*/
 }
