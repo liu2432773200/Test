afc6dfadb9cda58242b9271ffc2c85a25953bf5a
liu <lyh_liuyonghe@163.com>
2018-08-22 22:38:23 +0800

    Revert "111"
    This reverts commit eb007f673501f4174ccc9fafe18be0bb5306faf5.

---
+++ src/main/java/cn/entity/User.java
@@ -0,0 +1,39 @@
+package cn.entity;
+
+public class User {
+	private int b_id;
+	private String b_name;
+	private String b_author;
+	private String b_time;
+	private String b_type;
+	public int getB_id() {
+		return b_id;
+	}
+	public void setB_id(int b_id) {
+		this.b_id = b_id;
+	}
+	public String getB_name() {
+		return b_name;
+	}
+	public void setB_name(String b_name) {
+		this.b_name = b_name;
+	}
+	public String getB_author() {
+		return b_author;
+	}
+	public void setB_author(String b_author) {
+		this.b_author = b_author;
+	}
+	public String getB_time() {
+		return b_time;
+	}
+	public void setB_time(String b_time) {
+		this.b_time = b_time;
+	}
+	public String getB_type() {
+		return b_type;
+	}
+	public void setB_type(String b_type) {
+		this.b_type = b_type;
+	}
+}
---
+++ src/main/java/cn/service/UserService.java
@@ -0,0 +1,23 @@
+package cn.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import cn.entity.User;
+
+@Service
+public class UserService {
+	@Autowired
+	private JdbcTemplate jdbctemp;
+	
+	public int add(String name){
+		return jdbctemp.update("INSERT INTO `bookmanage`(b_name) VALUE(?)",name);
+	}
+	public List<User> getAlls(){
+		return jdbctemp.query("SELECT *FROM `bookmanage`",new Object[]{}, new BeanPropertyRowMapper(User.class));
+	}
+}
